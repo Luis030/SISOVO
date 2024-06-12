abd197dc0794b24cf4d3ea3148658bae0ab0b268
Luis030 <164172346+Luis030@users.noreply.github.com>
2024-06-12 17:09:36 -0300

    Diseño y linkeo de pestañas.

--- Pagina Principal/clases.php
+++ Pagina Principal/clases.php
@@ -1,23 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inicio</title>
+    <title>Clases</title>
+    <link rel="stylesheet" href="css/estilo.css?a">
     <meta http-equiv="Expires" content="0">
     <meta http-equiv="Last-Modified" content="0">
     <meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
     <meta http-equiv="Pragma" content="no-cache">
-    <link rel="stylesheet" href="css/estilo.css?a">
 </head>
 <body>
     <?php
     include("php/cuerpo.php");
     ?>
-    <div class="content">
-        <h1>Bienvenido, USER</h1>
-        <h2>Lunes 90 Octubre</h2>
-        <p>20:55</p>
-    </div>
 </body>
 </html>
---
+++ Pagina Principal/php/cuerpo.php
@@ -0,0 +1,15 @@
+<div class="header">
+        20:55
+        USER
+        <a href="#">⚙️</a>
+</div>
+<div class="sidebar">
+        <h2>Menú</h2>
+        <ul>
+            <li><a href="index.php">Inicio</a></li>
+            <li><a href="clases.php">Clases</a></li>
+            <li><a href="#">Horario</a></li>
+            <li><a href="informes.php">Informes</a></li>
+        </ul>
+        <a href="#" class="aboutus">About Us</a>
+</div>
