7272e85e7f6725323ae81e840a26a180f4cf9296
Vhaur <164172097+Vhaur@users.noreply.github.com>
2024-06-14 20:25:02 -0300

    Avances en la página principal
    Se agrego el header, y se esta trabajando en el main.

--- Pagina Principal/index.php
+++ Pagina Principal/index.php
@@ -3,9 +3,26 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <title>Centro CER</title>
+        <link rel="stylesheet" href="Diseño/styleindex.css">
     </head>
     <body>
-        <p>hola</p>
-    </body>
+        <div id="header">
+            <button id="IMGButton">
+                <img src="Diseño/IMG/config.png" alt="Config" id="headIMG">
+            </button>
+            <button id="headInfo">Servicios</button>
+            <button id="headInfo">Colecciones</button>
+            <a href="index.php" id="titleCER">
+                <h1>CER</h1>
+            </a>
+            <button id="headInfo">Alumnos</button>
+            <button id="headInfo">Contactos</button>
+        </div>
+        <div id="main">
+            <h1 id="mainTitle">Centro de Reeducación CER</h1>
+            <h2 id="mainTextMiddle">Para una educación mejor.</h2>
+            <p id="textIndex">En nuestra institución contamos con una gran varidad<br> de profesores especializados en diferentes áreas.</p>
+        </div>
+    </body>     
 </html>
