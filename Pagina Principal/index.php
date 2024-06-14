5177011037dca909964610612ce8f9a6c8c015fb
Vhaur <164172097+Vhaur@users.noreply.github.com>
2024-06-14 19:22:40 -0300

    Pagina Principal Inicio

--- Pagina Principal/index.php
+++ Pagina Principal/index.php
@@ -3,21 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inicio</title>
-    <meta http-equiv="Expires" content="0">
-    <meta http-equiv="Last-Modified" content="0">
-    <meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
-    <meta http-equiv="Pragma" content="no-cache">
-    <link rel="stylesheet" href="css/estilo.css?a">
+    <title>Document</title>
 </head>
 <body>
-    <?php
-    include("php/cuerpo.php");
-    ?>
-    <div class="content">
-        <h1>Bienvenido, USER</h1>
-        <h2>Lunes 90 Octubre</h2>
-        <p>20:55</p>
-    </div>
+    
 </body>
 </html>
