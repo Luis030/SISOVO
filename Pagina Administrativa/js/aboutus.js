b6329240a6ea3087d6ee0328652a6e45de03aacc
Luis030 <164172346+Luis030@users.noreply.github.com>
2024-06-14 20:13:00 -0300

    About us

--- Pagina Administrativa/js/aboutus.js
+++ Pagina Administrativa/js/aboutus.js
@@ -1,3 +1,17 @@
 window.onload = () => {
     
+        const abrirabout = document.getElementById('aboutus');
+        const overlay = document.getElementById('overlay');
+        const cerrarabout = document.getElementById('cerrarabout');
+      
+        abrirabout.addEventListener('click', function() {
+          overlay.style.display = 'block';
+          document.body.style.overflow = 'hidden';
+        });
+      
+        cerrarabout.addEventListener('click', function() {
+          overlay.style.display = 'none';
+          document.body.style.overflow = 'auto';
+        });
+      
 }
--- Pagina Administrativa/php/cuerpo.php
+++ Pagina Administrativa/php/cuerpo.php
@@ -1,3 +1,4 @@
+<script src="js/aboutus.js"></script>
 <div class="header">
         20:55
         USER
@@ -11,5 +12,25 @@
             <li><a href="#">Horario</a></li>
             <li><a href="informes.php">Informes</a></li>
         </ul>
-        <a href="#" class="aboutus">About Us</a>
+        <a href="#" class="aboutus" id="aboutus">About Us</a>
+</div>
+<div id="overlay" class="overlay">
+  <div class="contenido-overlay">
+    <button id="cerrarabout">Cerrar</button>
+    <div class="contenido-imagenes">
+        <div class="div-imagen">
+                <img src="https://concepto.de/wp-content/uploads/2015/03/paisaje-e1549600034372.jpg" alt="Luis Sosa" id="img1">
+                <h1>Luis Sosa</h1>
+        </div>
+        <div class="div-imagen">
+                <img src="https://www.shutterstock.com/image-illustration/view-waterfall-forest-600nw-2327645439.jpg" alt="Luis Sosa" id="img2">
+                <h1>Federico Simonelli</h1>
+        </div>
+        <div class="div-imagen">
+                <img src="https://cc-prod.scene7.com/is/image/CCProdAuthor/7%20AdobeStock_335413355?$png$&jpegSize=200&wid=960" alt="Luis Sosa" id="img3">
+                <h1>Ian Volpi</h1>
+        </div>
+    </div>
+    <p>Somos estudiante de informatica de tercer año de UTU, y esta aplicación fue desarrollada por motivo de nuestro proyecto de fin de año</p>
+  </div>
 </div>
