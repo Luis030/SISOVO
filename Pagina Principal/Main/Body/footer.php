a09a76f8f3a169f29af30c21888003272308d653
Vhaur <164172097+Vhaur@users.noreply.github.com>
2024-07-16 22:39:25 -0300

    Creado el primer diseño del "Sobre nosotros" de la página principal

--- Pagina Principal/Main/Body/footer.php
+++ Pagina Principal/Main/Body/footer.php
@@ -1,3 +1,3 @@
 <div id="footer">
-        <p id="footerInfo">&copy;2024 Centro CER | Developed by &copy;SISOVO Corp. | <a href="" id="footerAboutUs">Sobre nosotros</a></p>
+        <p id="footerInfo">&copy;2024 Centro CER | Developed by &copy;SISOVO Corp. | <a href="../Sobre nosotros" id="footerAboutUs"  target="_blank">Sobre nosotros</a></p>
 </div>
---
+++ Pagina Principal/Sobre nosotros/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Sobre nosotros</title>
+        <link rel="stylesheet" href="Diseño/style.css">
+    </head>
+    <body>
+        <h1>SISOVO Corporation</h1>
+        <div id="pic">
+            <div id="pics">
+                <div id="fotico">
+                    <img src="Diseño/IMG/prueba.jpg" alt="Luis Sosa">
+                </div>
+                <p>Luis Sosa</p>
+            </div>
+            <div id="pics">
+                <div id="fotico">
+                    <img src="Diseño/IMG/prueba.jpg" alt="Federico Simonelli">
+                </div>
+                <p>Federico Simonelli</p>
+            </div>
+            <div id="pics">
+                <div id="fotico">
+                    <img src="Diseño/IMG/prueba.jpg" alt="Ian Volpi">
+                </div>
+                <p>Ian Volpi</p>
+            </div>
+        </div>
+        <div id="infoNosotros">
+            <h2>Sobre nosotros</h2>
+            <p id="infotext">Somos estudiantes de 3ero de informática de la Escuela Superior Catalina Harriague de Castaños(UTU),<br> nos centramos en el diseño y desarrollo de páginas web, usando estructura HTML y lenguajes de programación<br>tales como PHP, JavaScript, entre otros.
+            </p>
+        </div>
+    </body>
+</html>
