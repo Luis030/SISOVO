e10cace264e516672b6ea9217c82284224e62350
Luis030 <164172346+Luis030@users.noreply.github.com>
2024-09-09 20:20:28 -0300

    añadir rapidamente patologias y especialidades
    Funcionalidad terminada para añadir rápidamente patologías o especialidades desde sus respectivos formularios, ambos formularios usan un javascript reutilizable que almacena las patologías/especialidades y las procesa en php dependiendo de su tipo.

--- Pagina Administrativa/js/overlay.js
+++ Pagina Administrativa/js/overlay.js
@@ -1,9 +1,16 @@
-
+document.addEventListener('keydown', (event) => {
+    if(event.key === 'Escape'){
+        const patOverlayDiv = document.getElementById('agregar-overlay');
+        if(patOverlayDiv.style.display == "block"){
+            patOverlayDiv.style.display = 'none';
+        }
+    }
+})
 function patOverlay(){
-    const patOverlayDiv = document.getElementById('patologias-overlay');
+    const patOverlayDiv = document.getElementById('agregar-overlay');
     patOverlayDiv.style.display = 'block';
 }
 function cerrarPatOverlay(){
-    const patOverlayDiv = document.getElementById('patologias-overlay');
+    const patOverlayDiv = document.getElementById('agregar-overlay');
     patOverlayDiv.style.display = 'none';
 }
