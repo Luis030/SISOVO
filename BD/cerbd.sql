d966d5d437db24fb9d0ebac64171212f4e30b972
Luis030 <164172346+Luis030@users.noreply.github.com>
2024-09-27 22:22:52 -0300

    Update cerbd.sql

--- BD/cerbd.sql
+++ BD/cerbd.sql
@@ -423,6 +423,61 @@ INSERT INTO `usuarios` (`ID_Usuario`, `Nombre`, `Cedula`, `Contraseña`, `Tipo`,
 (23, 'Ian Andrés Volpi Samit', 56499637, '$2y$10$AAV6SsgVfLAqIY5LgHxVf.b0CtVvjtzprcr58RqCD9Qhb82P4pyqi', 'admin', 1),
 (24, 'Luis Manuel Sosa Berroa', 56777350, '$2y$10$AEGAlZvNY1avPNO2yiQzcO4JgDlSCg2BA7kQ1e5dIEn3Np3m6hYvy', 'docente', 1);
 
+
+
+GRANT ALL PRIVILEGES ON cerbd.* TO cer identified by 'clinicacer';
+grant insert on cerbd.alumnos to administrador identified by 'admin@pass';
+grant insert on cerbd.docentes to administrador;
+grant insert on cerbd.clase to administrador;
+grant insert on cerbd.patologias to administrador;
+grant insert on cerbd.especializaciones to administrador;
+grant insert on cerbd.usuarios to administrador;
+grant insert on cerbd.ocupacion to administrador;
+grant insert on cerbd.patologia_alumno to administrador;
+grant insert on cerbd.especializacion_docente to administrador;
+grant insert on cerbd.llegada_docente to administrador;
+grant insert on cerbd.alumnos_clase to administrador;
+
+grant select on cerbd.alumnos to administrador;
+grant select on cerbd.docentes to administrador;
+grant select on cerbd.clase to administrador;
+grant select on cerbd.patologias to administrador;
+grant select on cerbd.especializaciones to administrador;
+grant select on cerbd.usuarios to administrador;
+grant select on cerbd.ocupacion to administrador;
+grant select on cerbd.patologia_alumno to administrador;
+grant select on cerbd.especializacion_docente to administrador;
+grant select on cerbd.llegada_docente to administrador;
+grant select on cerbd.alumnos_clase to administrador;
+grant select on cerbd.informes to administrador;
+
+grant delete on cerbd.patologias to administrador;
+grant delete on cerbd.especializaciones to administrador;
+grant delete on cerbd.clase to administrador;
+grant delete on cerbd.alumnos_clase to administrador;
+
+grant update(Nombre, apellido, cedula, estado, fecha_nac, apellido, celular_padres, mail_padres) on cerbd.alumnos to administrador;
+grant update(Nombre, apellido, cedula, mail, celular, estado, fecha_nac) on cerbd.docentes to administrador;
+grant update(Nombre, inicio, final, dia, estado) on cerbd.clase to administrador;
+grant update(Nombre) on cerbd.patologias to administrador;
+grant update(Nombre) on cerbd.especializaciones to administrador;
+grant update(Nombre, cedula, estado) on cerbd.usuarios to administrador;
+grant update(Nombre, estado) on cerbd.ocupacion to administrador;
+
+grant select on cerbd.informes to docente identified by 'docente@pass';
+grant select on cerbd.usuarios to docente;
+grant select on cerbd.alumnos to docente;
+grant select on cerbd.asistencias to docente;
+grant select on cerbd.patologia_alumno to docente;
+grant select on cerbd.clase to docente;
+grant select on cerbd.alumnos_clase to docente;
+grant select on cerbd.patologias to docente;
+grant insert on cerbd.informes to docente;
+
+grant update on cerbd.informes to docente;
+
+grant select on cerbd.informes to alumno identified by 'alumno@pass';
+grant select on cerbd.usuarios to alumno;
 --
 -- Índices para tablas volcadas
 --
