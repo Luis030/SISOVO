3baed32cfd9622f71bb346a703ab10a881d69106
Luis030 <164172346+Luis030@users.noreply.github.com>
2024-10-04 17:23:45 -0300

    Update cerbd.sql

--- BD/cerbd.sql
+++ BD/cerbd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 30-09-2024 a las 01:16:36
+-- Tiempo de generación: 04-10-2024 a las 22:22:59
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -65,78 +65,74 @@ INSERT INTO `alumnos` (`ID_Alumno`, `ID_Usuario`, `Nombre`, `Apellido`, `Cedula`
 CREATE TABLE `alumnos_clase` (
   `ID_Clase` int(11) NOT NULL,
   `ID_Alumno` int(11) NOT NULL,
-  `Fecha` date NOT NULL
+  `Fecha` date NOT NULL,
+  `Asistio` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `alumnos_clase`
 --
 
-INSERT INTO `alumnos_clase` (`ID_Clase`, `ID_Alumno`, `Fecha`) VALUES
-(2, 1, '0000-00-00'),
-(2, 5, '0000-00-00'),
-(2, 11, '0000-00-00'),
-(2, 3, '0000-00-00'),
-(2, 2, '0000-00-00'),
-(3, 11, '0000-00-00'),
-(3, 8, '0000-00-00'),
-(3, 4, '0000-00-00'),
-(3, 2, '0000-00-00'),
-(3, 5, '0000-00-00'),
-(4, 10, '0000-00-00'),
-(4, 6, '0000-00-00'),
-(4, 1, '0000-00-00'),
-(4, 4, '0000-00-00'),
-(4, 11, '0000-00-00'),
-(5, 10, '0000-00-00'),
-(5, 1, '0000-00-00'),
-(5, 5, '0000-00-00'),
-(5, 3, '0000-00-00'),
-(5, 8, '0000-00-00'),
-(6, 11, '0000-00-00'),
-(6, 3, '0000-00-00'),
-(6, 2, '0000-00-00'),
-(6, 5, '0000-00-00'),
-(6, 1, '0000-00-00'),
-(7, 11, '0000-00-00'),
-(7, 9, '0000-00-00'),
-(7, 2, '0000-00-00'),
-(7, 1, '0000-00-00'),
-(7, 5, '0000-00-00'),
-(8, 7, '0000-00-00'),
-(8, 2, '0000-00-00'),
-(8, 1, '0000-00-00'),
-(8, 10, '0000-00-00'),
-(8, 4, '0000-00-00'),
-(9, 10, '0000-00-00'),
-(9, 7, '0000-00-00'),
-(9, 1, '0000-00-00'),
-(9, 3, '0000-00-00'),
-(9, 5, '0000-00-00'),
-(10, 11, '0000-00-00'),
-(10, 8, '0000-00-00'),
-(10, 2, '0000-00-00'),
-(10, 1, '0000-00-00'),
-(10, 5, '0000-00-00'),
-(11, 10, '0000-00-00'),
-(11, 2, '0000-00-00'),
-(11, 1, '0000-00-00'),
-(11, 11, '0000-00-00'),
-(11, 4, '0000-00-00');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `asistencias`
---
-
-CREATE TABLE `asistencias` (
-  `ID_Asistencia` int(11) NOT NULL,
-  `ID_Clase` int(11) NOT NULL,
-  `ID_Alumno` int(11) NOT NULL,
-  `Fecha` date NOT NULL,
-  `Asistio` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT INTO `alumnos_clase` (`ID_Clase`, `ID_Alumno`, `Fecha`, `Asistio`) VALUES
+(2, 1, '0000-00-00', NULL),
+(2, 5, '0000-00-00', NULL),
+(2, 11, '0000-00-00', NULL),
+(2, 3, '0000-00-00', NULL),
+(2, 2, '0000-00-00', NULL),
+(3, 11, '0000-00-00', NULL),
+(3, 8, '0000-00-00', NULL),
+(3, 4, '0000-00-00', NULL),
+(3, 2, '0000-00-00', NULL),
+(3, 5, '0000-00-00', NULL),
+(4, 10, '0000-00-00', NULL),
+(4, 6, '0000-00-00', NULL),
+(4, 1, '0000-00-00', NULL),
+(4, 4, '0000-00-00', NULL),
+(4, 11, '0000-00-00', NULL),
+(5, 10, '0000-00-00', NULL),
+(5, 1, '0000-00-00', NULL),
+(5, 5, '0000-00-00', NULL),
+(5, 3, '0000-00-00', NULL),
+(5, 8, '0000-00-00', NULL),
+(8, 7, '0000-00-00', NULL),
+(8, 2, '0000-00-00', NULL),
+(8, 1, '0000-00-00', NULL),
+(8, 10, '0000-00-00', NULL),
+(8, 4, '0000-00-00', NULL),
+(9, 10, '0000-00-00', NULL),
+(9, 7, '0000-00-00', NULL),
+(9, 1, '0000-00-00', NULL),
+(9, 3, '0000-00-00', NULL),
+(9, 5, '0000-00-00', NULL),
+(10, 11, '0000-00-00', NULL),
+(10, 8, '0000-00-00', NULL),
+(10, 2, '0000-00-00', NULL),
+(10, 1, '0000-00-00', NULL),
+(10, 5, '0000-00-00', NULL),
+(11, 10, '0000-00-00', NULL),
+(11, 2, '0000-00-00', NULL),
+(11, 1, '0000-00-00', NULL),
+(11, 11, '0000-00-00', NULL),
+(11, 4, '0000-00-00', NULL),
+(8, 5, '0000-00-00', NULL),
+(8, 8, '0000-00-00', NULL),
+(8, 9, '0000-00-00', NULL),
+(8, 3, '0000-00-00', NULL),
+(8, 11, '0000-00-00', NULL),
+(9, 6, '0000-00-00', NULL),
+(9, 8, '0000-00-00', NULL),
+(10, 4, '0000-00-00', NULL),
+(10, 6, '0000-00-00', NULL),
+(10, 7, '0000-00-00', NULL),
+(10, 9, '0000-00-00', NULL),
+(10, 10, '0000-00-00', NULL),
+(10, 3, '0000-00-00', NULL),
+(5, 4, '0000-00-00', NULL),
+(5, 6, '0000-00-00', NULL),
+(5, 9, '0000-00-00', NULL),
+(5, 11, '0000-00-00', NULL),
+(5, 2, '0000-00-00', NULL),
+(5, 7, '0000-00-00', NULL);
 
 -- --------------------------------------------------------
 
@@ -161,14 +157,36 @@ CREATE TABLE `clase` (
 INSERT INTO `clase` (`ID_Clase`, `ID_Docente`, `Nombre`, `Dia`, `Inicio`, `Final`, `Estado`) VALUES
 (2, 13, 'Matemáticas', 'Lunes', '08:00:00', '09:30:00', 1),
 (3, 2, 'Historia', 'Martes', '10:00:00', '11:30:00', 1),
-(4, 3, 'Química', 'Miércoles', '13:00:00', '14:30:00', 1),
-(5, 4, 'Física', 'Jueves', '09:00:00', '10:30:00', 1),
-(6, 5, 'Literatura', 'Viernes', '14:00:00', '15:30:00', 1),
-(7, 6, 'Geografía', 'Lunes', '11:00:00', '12:30:00', 1),
-(8, 7, 'Biología', 'Martes', '13:30:00', '15:00:00', 1),
+(4, 3, 'Área de lengua', 'Miércoles', '13:00:00', '14:30:00', 1),
+(5, 4, 'Levitacion', 'Jueves', '09:00:00', '10:30:00', 1),
+(8, 7, 'Levitacion', 'Martes', '13:30:00', '15:00:00', 1),
 (9, 8, 'Educación Física', 'Miércoles', '16:00:00', '17:30:00', 1),
 (10, 9, 'Arte', 'Jueves', '12:00:00', '13:30:00', 1),
 (11, 10, 'Inglés', 'Viernes', '08:30:00', '10:00:00', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `dias`
+--
+
+CREATE TABLE `dias` (
+  `ID_Dia` int(11) NOT NULL,
+  `Nombre` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `dias_clase`
+--
+
+CREATE TABLE `dias_clase` (
+  `ID_Clase` int(11) NOT NULL,
+  `ID_Dia` int(11) NOT NULL,
+  `Inicio` time NOT NULL,
+  `Final` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -185,7 +203,6 @@ CREATE TABLE `docentes` (
   `Mail` varchar(60) NOT NULL,
   `Celular` varchar(9) DEFAULT NULL,
   `Fecha_Nac` date DEFAULT NULL,
-  `ID_Ocupacion` int(11) DEFAULT NULL,
   `Estado` tinyint(1) DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -193,19 +210,19 @@ CREATE TABLE `docentes` (
 -- Volcado de datos para la tabla `docentes`
 --
 
-INSERT INTO `docentes` (`ID_Docente`, `ID_Usuario`, `Nombre`, `Apellido`, `Cedula`, `Mail`, `Celular`, `Fecha_Nac`, `ID_Ocupacion`, `Estado`) VALUES
-(1, 11, 'Ana', 'Ramírez', 69647322, 'ana.ramírez@example.com', '097958413', '1994-07-28', 1, 1),
-(2, 12, 'Pedro', 'Gómez', 26673841, 'pedro.gómez@example.com', '097816287', '1991-08-14', 2, 1),
-(3, 13, 'Laura', 'Fernández', 73882441, 'laura.fernández@example.com', '095430551', '2000-11-03', 3, 1),
-(4, 14, 'Santiago', 'Morales', 61377294, 'santiago.morales@example.com', '098171709', '2003-10-26', 4, 1),
-(5, 15, 'Claudia', 'Ruiz', 92692705, 'claudia.ruiz@example.com', '096836842', '2002-04-05', 5, 1),
-(6, 16, 'Roberto', 'Torres', 52837341, 'roberto.torres@example.com', '092898511', '1986-11-23', 6, 1),
-(7, 17, 'María', 'López', 16156404, 'maría.lópez@example.com', '098583986', '2000-09-07', 7, 1),
-(8, 18, 'Carlos', 'Méndez', 45659784, 'carlos.méndez@example.com', '091633174', '1998-12-04', 8, 1),
-(9, 19, 'Paula', 'Gutiérrez', 71192529, 'paula.gutiérrez@example.com', '098201334', '2004-08-19', 9, 1),
-(10, 20, 'Jorge', 'Herrera', 15918386, 'jorge.herrera@example.com', '095486464', '1985-02-05', 10, 1),
-(13, 24, 'Luis Manuel', 'Sosa Berroa', 56777350, 'LManuelSosa@gmail.com', '092504454', '2007-03-27', 5, 1),
-(14, 25, 'Ian Andrés', 'Volpi Zamit', 56499637, 'nopetif@gmail.com', '09250944', '2007-03-27', 6, 1);
+INSERT INTO `docentes` (`ID_Docente`, `ID_Usuario`, `Nombre`, `Apellido`, `Cedula`, `Mail`, `Celular`, `Fecha_Nac`, `Estado`) VALUES
+(1, 11, 'Ana', 'Ramírez', 69647322, 'ana.ramírez@example.com', '097958413', '1994-07-28', 1),
+(2, 12, 'Pedro', 'Gómez', 26673841, 'pedro.gómez@example.com', '097816287', '1991-08-14', 1),
+(3, 13, 'Laura', 'Fernández', 73882441, 'laura.fernández@example.com', '095430551', '2000-11-03', 1),
+(4, 14, 'Santiago', 'Morales', 61377294, 'santiago.morales@example.com', '098171709', '2003-10-26', 1),
+(5, 15, 'Claudia', 'Ruiz', 92692705, 'claudia.ruiz@example.com', '096836842', '2002-04-05', 1),
+(6, 16, 'Roberto', 'Torres', 52837341, 'roberto.torres@example.com', '092898511', '1986-11-23', 1),
+(7, 17, 'María', 'López', 16156404, 'maría.lópez@example.com', '098583986', '2000-09-07', 1),
+(8, 18, 'Carlos', 'Méndez', 45659784, 'carlos.méndez@example.com', '091633174', '1998-12-04', 1),
+(9, 19, 'Paula', 'Gutiérrez', 71192529, 'paula.gutiérrez@example.com', '098201334', '2004-08-19', 1),
+(10, 20, 'Jorge', 'Herrera', 15918386, 'jorge.herrera@example.com', '095486464', '1985-02-05', 1),
+(13, 24, 'Luis Manuel', 'Sosa Berroa', 56777350, 'LManuelSosa@gmail.com', '092504454', '2007-03-27', 1),
+(14, 25, 'Ian Andrés', 'Volpi Zamit', 56499637, 'nopetif@gmail.com', '09250944', '2007-03-27', 1);
 
 -- --------------------------------------------------------
 
@@ -247,7 +264,8 @@ INSERT INTO `especializaciones` (`ID_Especializacion`, `Nombre`, `ID_Ocupacion`)
 (21, 'Orientación vocacional y profesional', 2),
 (22, 'Tutoría académica personalizada', 10),
 (23, 'Desarrollo de habilidades de estudio', 10),
-(24, 'Orientación en aprendizaje socioemocional', 10);
+(24, 'Orientación en aprendizaje socioemocional', 10),
+(25, 'auxiliar', 7);
 
 -- --------------------------------------------------------
 
@@ -298,14 +316,15 @@ INSERT INTO `informes` (`ID_Informe`, `ID_Docente`, `ID_Alumno`, `Titulo`, `Obse
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `llegada_docente`
+-- Estructura de tabla para la tabla `lista_docente`
 --
 
-CREATE TABLE `llegada_docente` (
-  `ID_Llegada` int(11) NOT NULL,
+CREATE TABLE `lista_docente` (
+  `ID_Asistencia` int(11) NOT NULL,
   `ID_Docente` int(11) NOT NULL,
   `Fecha` date NOT NULL,
-  `Hora` time NOT NULL
+  `Hora` time NOT NULL,
+  `Tipo` varchar(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -445,14 +464,6 @@ ALTER TABLE `alumnos_clase`
   ADD KEY `ID_Alumno` (`ID_Alumno`);
 
 --
--- Indices de la tabla `asistencias`
---
-ALTER TABLE `asistencias`
-  ADD PRIMARY KEY (`ID_Asistencia`),
-  ADD KEY `ID_Clase` (`ID_Clase`),
-  ADD KEY `ID_Alumno` (`ID_Alumno`);
-
---
 -- Indices de la tabla `clase`
 --
 ALTER TABLE `clase`
@@ -460,12 +471,24 @@ ALTER TABLE `clase`
   ADD KEY `ID_Docente` (`ID_Docente`);
 
 --
+-- Indices de la tabla `dias`
+--
+ALTER TABLE `dias`
+  ADD PRIMARY KEY (`ID_Dia`);
+
+--
+-- Indices de la tabla `dias_clase`
+--
+ALTER TABLE `dias_clase`
+  ADD KEY `ID_Dia` (`ID_Dia`),
+  ADD KEY `ID_Clase` (`ID_Clase`);
+
+--
 -- Indices de la tabla `docentes`
 --
 ALTER TABLE `docentes`
   ADD PRIMARY KEY (`ID_Docente`),
-  ADD KEY `ID_Usuario` (`ID_Usuario`),
-  ADD KEY `ID_Ocupacion` (`ID_Ocupacion`);
+  ADD KEY `ID_Usuario` (`ID_Usuario`);
 
 --
 -- Indices de la tabla `especializaciones`
@@ -490,10 +513,10 @@ ALTER TABLE `informes`
   ADD KEY `ID_Alumno` (`ID_Alumno`);
 
 --
--- Indices de la tabla `llegada_docente`
+-- Indices de la tabla `lista_docente`
 --
-ALTER TABLE `llegada_docente`
-  ADD PRIMARY KEY (`ID_Llegada`),
+ALTER TABLE `lista_docente`
+  ADD PRIMARY KEY (`ID_Asistencia`),
   ADD KEY `ID_Docente` (`ID_Docente`);
 
 --
@@ -534,16 +557,16 @@ ALTER TABLE `alumnos`
   MODIFY `ID_Alumno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
--- AUTO_INCREMENT de la tabla `asistencias`
---
-ALTER TABLE `asistencias`
-  MODIFY `ID_Asistencia` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT de la tabla `clase`
 --
 ALTER TABLE `clase`
   MODIFY `ID_Clase` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT de la tabla `dias`
+--
+ALTER TABLE `dias`
+  MODIFY `ID_Dia` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `docentes`
@@ -555,7 +578,7 @@ ALTER TABLE `docentes`
 -- AUTO_INCREMENT de la tabla `especializaciones`
 --
 ALTER TABLE `especializaciones`
-  MODIFY `ID_Especializacion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `ID_Especializacion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT de la tabla `informes`
@@ -564,10 +587,10 @@ ALTER TABLE `informes`
   MODIFY `ID_Informe` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT de la tabla `llegada_docente`
+-- AUTO_INCREMENT de la tabla `lista_docente`
 --
-ALTER TABLE `llegada_docente`
-  MODIFY `ID_Llegada` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `lista_docente`
+  MODIFY `ID_Asistencia` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `ocupacion`
@@ -605,24 +628,23 @@ ALTER TABLE `alumnos_clase`
   ADD CONSTRAINT `alumnos_clase_ibfk_3` FOREIGN KEY (`ID_Clase`) REFERENCES `clase` (`ID_Clase`) ON DELETE CASCADE;
 
 --
--- Filtros para la tabla `asistencias`
---
-ALTER TABLE `asistencias`
-  ADD CONSTRAINT `asistencias_ibfk_2` FOREIGN KEY (`ID_Alumno`) REFERENCES `alumnos` (`ID_Alumno`),
-  ADD CONSTRAINT `asistencias_ibfk_3` FOREIGN KEY (`ID_Clase`) REFERENCES `clase` (`ID_Clase`) ON DELETE CASCADE;
-
---
 -- Filtros para la tabla `clase`
 --
 ALTER TABLE `clase`
   ADD CONSTRAINT `clase_ibfk_1` FOREIGN KEY (`ID_Docente`) REFERENCES `docentes` (`ID_Docente`);
 
 --
+-- Filtros para la tabla `dias_clase`
+--
+ALTER TABLE `dias_clase`
+  ADD CONSTRAINT `dias_clase_ibfk_1` FOREIGN KEY (`ID_Dia`) REFERENCES `dias` (`ID_Dia`),
+  ADD CONSTRAINT `dias_clase_ibfk_2` FOREIGN KEY (`ID_Clase`) REFERENCES `clase` (`ID_Clase`);
+
+--
 -- Filtros para la tabla `docentes`
 --
 ALTER TABLE `docentes`
-  ADD CONSTRAINT `docentes_ibfk_1` FOREIGN KEY (`ID_Usuario`) REFERENCES `usuarios` (`ID_Usuario`),
-  ADD CONSTRAINT `docentes_ibfk_2` FOREIGN KEY (`ID_Ocupacion`) REFERENCES `ocupacion` (`ID_Ocupacion`);
+  ADD CONSTRAINT `docentes_ibfk_1` FOREIGN KEY (`ID_Usuario`) REFERENCES `usuarios` (`ID_Usuario`);
 
 --
 -- Filtros para la tabla `especializaciones`
@@ -645,10 +667,10 @@ ALTER TABLE `informes`
   ADD CONSTRAINT `informes_ibfk_2` FOREIGN KEY (`ID_Alumno`) REFERENCES `alumnos` (`ID_Alumno`);
 
 --
--- Filtros para la tabla `llegada_docente`
+-- Filtros para la tabla `lista_docente`
 --
-ALTER TABLE `llegada_docente`
-  ADD CONSTRAINT `llegada_docente_ibfk_1` FOREIGN KEY (`ID_Docente`) REFERENCES `docentes` (`ID_Docente`);
+ALTER TABLE `lista_docente`
+  ADD CONSTRAINT `lista_docente_ibfk_1` FOREIGN KEY (`ID_Docente`) REFERENCES `docentes` (`ID_Docente`);
 
 --
 -- Filtros para la tabla `patologia_alumno`
@@ -661,57 +683,3 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-GRANT ALL PRIVILEGES ON cerbd.* TO cer identified by 'clinicacer';
-grant insert on cerbd.alumnos to administrador identified by 'admin@pass';
-grant insert on cerbd.docentes to administrador;
-grant insert on cerbd.clase to administrador;
-grant insert on cerbd.patologias to administrador;
-grant insert on cerbd.especializaciones to administrador;
-grant insert on cerbd.usuarios to administrador;
-grant insert on cerbd.ocupacion to administrador;
-grant insert on cerbd.patologia_alumno to administrador;
-grant insert on cerbd.especializacion_docente to administrador;
-grant insert on cerbd.llegada_docente to administrador;
-grant insert on cerbd.alumnos_clase to administrador;
-
-grant select on cerbd.alumnos to administrador;
-grant select on cerbd.docentes to administrador;
-grant select on cerbd.clase to administrador;
-grant select on cerbd.patologias to administrador;
-grant select on cerbd.especializaciones to administrador;
-grant select on cerbd.usuarios to administrador;
-grant select on cerbd.ocupacion to administrador;
-grant select on cerbd.patologia_alumno to administrador;
-grant select on cerbd.especializacion_docente to administrador;
-grant select on cerbd.llegada_docente to administrador;
-grant select on cerbd.alumnos_clase to administrador;
-grant select on cerbd.informes to administrador;
-
-grant delete on cerbd.patologias to administrador;
-grant delete on cerbd.especializaciones to administrador;
-grant delete on cerbd.clase to administrador;
-grant delete on cerbd.alumnos_clase to administrador;
-
-grant update(Nombre, apellido, cedula, estado, fecha_nac, apellido, celular_padres, mail_padres) on cerbd.alumnos to administrador;
-grant update(Nombre, apellido, cedula, mail, celular, estado, fecha_nac) on cerbd.docentes to administrador;
-grant update(Nombre, inicio, final, dia, estado) on cerbd.clase to administrador;
-grant update(Nombre) on cerbd.patologias to administrador;
-grant update(Nombre) on cerbd.especializaciones to administrador;
-grant update(Nombre, cedula, estado) on cerbd.usuarios to administrador;
-grant update(Nombre, estado) on cerbd.ocupacion to administrador;
-
-grant select on cerbd.informes to docente identified by 'docente@pass';
-grant select on cerbd.usuarios to docente;
-grant select on cerbd.alumnos to docente;
-grant select on cerbd.asistencias to docente;
-grant select on cerbd.patologia_alumno to docente;
-grant select on cerbd.clase to docente;
-grant select on cerbd.alumnos_clase to docente;
-grant select on cerbd.patologias to docente;
-grant insert on cerbd.informes to docente;
-
-grant update on cerbd.informes to docente;
-
-grant select on cerbd.informes to alumno identified by 'alumno@pass';
-grant select on cerbd.usuarios to alumno;
