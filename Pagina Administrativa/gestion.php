acc3895e81bd95b8536afb0a54586a173f11e573
IanVol10 <164171997+IanVol10@users.noreply.github.com>
2024-10-09 01:56:53 -0300

    gestion

---
+++ Pagina Administrativa/gestion.php
@@ -0,0 +1,33 @@
+<?php
+include("../BD/conexionbd.php");
+include("php/funciones.php");
+?>
+<?php
+include("php/header_sidebar.php");
+?>
+
+<! ref a estilos 
+!>
+<link rel="stylesheet" href="css/estilogestion.css">
+<script src="JS/cargarselect.js"></script>
+<script src="JS/overlay.js"></script>
+<script src="js/overlayreutilizable.js"></script>
+
+<! divs  !> 
+<div class="Graficas">
+    
+    <div class="botones">
+    <a href="pepe" class="boton-inicio">Inicio</a>
+    <a href="añadirdocente.php" class="boton-inicio">Docentes</a>
+    <a href="añadiralumno.php" class="boton-inicio">Alumnos</a>
+    <a href="añadirdocente.php" class="boton-inicio">Informes</a>
+    <a href="añadiralumno.php" class="boton-inicio">Clases</a>
+    <a href="añadirdocente.php" class="boton-inicio">Elementos</a>
+    </div>
+
+</div>
+
+
+<?php
+include("php/footer.php");
+?>
