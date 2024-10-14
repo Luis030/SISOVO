1bca3c1c3104d291dc4d5b021c216ca887661cec
Luis030 <164172346+Luis030@users.noreply.github.com>
2024-10-14 17:27:00 -0300

    Filtros para la tabla lista docente :100:

--- Pagina Administrativa/js/filtros.js
+++ Pagina Administrativa/js/filtros.js
@@ -26,26 +26,87 @@ function filtrarListaDocente(tipo){
 function filtrosAvanzadosListaDocente(){
     Swal.fire({
         title: "Filtros avanzados",
-        focusConfirm: true,
+        showCancelButton: true,
         confirmButtonText: "Aceptar",
+        cancelButtonColor: "#d33",
         cancelButtonText: "Cancelar",
         html: `
+        <div class="filtros-div">
+        Docente:
+        <input type="text" id="docente-filtro" class="avanzado" placeholder="Nombre o cedula"><br><br>
         Fecha:
-        <input type="date" class="avanzado"><br><br>
-
-        Rango de fechas: <br> <br>
-         <input type="date" class="avanzado" disabled> <input type="date" class="avanzado" disabled>
-        <br><br>
+        <input type="date" id="fecha-filtro" class="avanzado"><br><br>
         Tipo: <select id="selectAvanzado">
             <option value=""></option>
             <option value="E">Entrada</option>
             <option value="S">Salida</option>
         </select>
         <br><br>
-        Hora: <input type="time" class="avanzado">
+        Hora: <input type="time" id="hora-avanzado" class="avanzado">
         <br><br>
-        Rango de horas: <br><br>
-        <input type="time" class="avanzado" disabled> <input type="time" class="avanzado" disabled>
+      </div>
+      <div class="filtros-div">
+          Rango de fechas: <br> <br>
+          Min: <input type="date" id="fechas-filtro-1" class="avanzado"><br><br> Max: <input type="date" id="fechas-filtro-2" class="avanzado">
+          <br><br>
+          
+          
+          Rango de horas: <br><br>
+          Min: <input type="time" id="horas-filtro-1" class="avanzado"><br><br>Max: <input type="time" id="horas-filtro-2" class="avanzado">
+      </div>
         `
+    }).then((resultado) => {
+      if(resultado.isConfirmed){
+        let docente = "";
+        let fecha = "";
+        let tipo = "";
+        let hora = "";
+        let rangofechas1 = "";
+        let rangofechas2 = "";
+        let rangohoras1 = "";
+        let rangohoras2 = "";
+        const ValorDocente = document.getElementById('docente-filtro').value;
+        const ValorFecha = document.getElementById('fecha-filtro').value;
+        const ValorTipo = document.getElementById('selectAvanzado').value;
+        const ValorHora = document.getElementById('hora-avanzado').value;
+        const ValorFechas1 = document.getElementById('fechas-filtro-1').value;
+        const ValorFechas2 = document.getElementById('fechas-filtro-2').value;
+        const ValorHoras1 = document.getElementById('horas-filtro-1').value;
+        const ValorHoras2 = document.getElementById('horas-filtro-2').value;
+        if(ValorDocente){
+          docente = ValorDocente
+        }
+        if(ValorFecha){
+          fecha = ValorFecha
+        }
+        if(ValorTipo){
+          tipo = ValorTipo
+        }
+        if(ValorHora){
+          hora = ValorHora
+        }
+        if(ValorFechas1){
+          fecha = "";
+          rangofechas1 = ValorFechas1;
+        }
+        if(ValorFechas2){
+          fecha = "";
+          rangofechas2 = ValorFechas2;
+        }
+        if(ValorHoras1){
+          hora = "";
+          rangohoras1 = ValorHoras1;
+        }
+        if(ValorHoras2){
+          hora = "";
+          rangohoras2 = ValorHoras2;
+        }
+        
+        tablas['listadoc'] = iniciarTabla('lista-docente', 'php/asistenciasdocentes.php?docente='+docente+'&&fecha='+fecha+'&&tipo='+tipo+'&&hora='+hora+'&&rangofecha1='+rangofechas1+'&&rangofecha2='+rangofechas2+'&&rangohora1='+rangohoras1+'&&rangohora2='+rangohoras2, columnasListaDocente(), "50vh", rollCallBackListaDocente);
+        const radios = document.querySelectorAll('.checkfiltro');
+        radios.forEach(radio => {
+            radio.checked = false;
+        });
+      }
     })
 }
--- Pagina Administrativa/php/asistenciasdocentes.php
+++ Pagina Administrativa/php/asistenciasdocentes.php
@@ -4,29 +4,83 @@ if(isset($_GET['time'])){
     $rango = $_GET['time'];
     switch($rango){
         case 'hoy':
-            $sql = "SELECT CONCAT(D.Nombre, ' ', D.Apellido) AS Docente, D.ID_Docente, D.Cedula, CASE WHEN LD.Tipo = 'E' THEN 'Entrada' WHEN LD.Tipo = 'S' THEN 'Salida' END AS Tipo, LD.Fecha, LD.Hora FROM docentes D JOIN lista_docente LD ON LD.ID_Docente = D.ID_Docente WHERE LD.Fecha=curdate() ORDER BY Fecha DESC, Hora DESC;";
+            $sql = "SELECT CONCAT(D.Nombre, ' ', D.Apellido) AS Docente, D.ID_Docente, D.Cedula, CASE WHEN LD.Tipo = 'E' THEN 'Entrada' WHEN LD.Tipo = 'S' THEN 'Salida' END AS Tipo, DATE_FORMAT(LD.Fecha, '%d/%m/%Y') AS Fecha, LD.Hora FROM docentes D JOIN lista_docente LD ON LD.ID_Docente = D.ID_Docente WHERE LD.Fecha=curdate() ORDER BY Fecha DESC, Hora DESC;";
             break;
         case '3d':
-            $sql = "SELECT CONCAT(D.Nombre, ' ', D.Apellido) AS Docente, D.ID_Docente, D.Cedula, CASE WHEN LD.Tipo = 'E' THEN 'Entrada' WHEN LD.Tipo = 'S' THEN 'Salida' END AS Tipo, LD.Fecha, LD.Hora FROM docentes D JOIN lista_docente LD ON LD.ID_Docente = D.ID_Docente WHERE LD.Fecha >= DATE_SUB(CURDATE(), INTERVAL 3 DAY) ORDER BY Fecha DESC, Hora DESC;";
+            $sql = "SELECT CONCAT(D.Nombre, ' ', D.Apellido) AS Docente, D.ID_Docente, D.Cedula, CASE WHEN LD.Tipo = 'E' THEN 'Entrada' WHEN LD.Tipo = 'S' THEN 'Salida' END AS Tipo, DATE_FORMAT(LD.Fecha, '%d/%m/%Y') AS Fecha, LD.Hora FROM docentes D JOIN lista_docente LD ON LD.ID_Docente = D.ID_Docente WHERE LD.Fecha >= DATE_SUB(CURDATE(), INTERVAL 3 DAY) ORDER BY Fecha DESC, Hora DESC;";
             break;
         case '7d':
-            $sql = "SELECT CONCAT(D.Nombre, ' ', D.Apellido) AS Docente, D.ID_Docente, D.Cedula, CASE WHEN LD.Tipo = 'E' THEN 'Entrada' WHEN LD.Tipo = 'S' THEN 'Salida' END AS Tipo, LD.Fecha, LD.Hora FROM docentes D JOIN lista_docente LD ON LD.ID_Docente = D.ID_Docente WHERE LD.Fecha >= DATE_SUB(CURDATE(), INTERVAL 7 DAY) ORDER BY Fecha DESC, Hora DESC;";
+            $sql = "SELECT CONCAT(D.Nombre, ' ', D.Apellido) AS Docente, D.ID_Docente, D.Cedula, CASE WHEN LD.Tipo = 'E' THEN 'Entrada' WHEN LD.Tipo = 'S' THEN 'Salida' END AS Tipo, DATE_FORMAT(LD.Fecha, '%d/%m/%Y') AS Fecha, LD.Hora FROM docentes D JOIN lista_docente LD ON LD.ID_Docente = D.ID_Docente WHERE LD.Fecha >= DATE_SUB(CURDATE(), INTERVAL 7 DAY) ORDER BY Fecha DESC, Hora DESC;";
             break;
         case '30d':
-            $sql = "SELECT CONCAT(D.Nombre, ' ', D.Apellido) AS Docente, D.ID_Docente, D.Cedula, CASE WHEN LD.Tipo = 'E' THEN 'Entrada' WHEN LD.Tipo = 'S' THEN 'Salida' END AS Tipo, LD.Fecha, LD.Hora FROM docentes D JOIN lista_docente LD ON LD.ID_Docente = D.ID_Docente WHERE LD.Fecha >= DATE_SUB(CURDATE(), INTERVAL 30 DAY) ORDER BY Fecha DESC, Hora DESC;";
+            $sql = "SELECT CONCAT(D.Nombre, ' ', D.Apellido) AS Docente, D.ID_Docente, D.Cedula, CASE WHEN LD.Tipo = 'E' THEN 'Entrada' WHEN LD.Tipo = 'S' THEN 'Salida' END AS Tipo, DATE_FORMAT(LD.Fecha, '%d/%m/%Y') AS Fecha, LD.Hora FROM docentes D JOIN lista_docente LD ON LD.ID_Docente = D.ID_Docente WHERE LD.Fecha >= DATE_SUB(CURDATE(), INTERVAL 30 DAY) ORDER BY Fecha DESC, Hora DESC;";
             break;
         case '6m':
-            $sql = "SELECT CONCAT(D.Nombre, ' ', D.Apellido) AS Docente, D.ID_Docente, D.Cedula, CASE WHEN LD.Tipo = 'E' THEN 'Entrada' WHEN LD.Tipo = 'S' THEN 'Salida' END AS Tipo, LD.Fecha, LD.Hora FROM docentes D JOIN lista_docente LD ON LD.ID_Docente = D.ID_Docente WHERE LD.Fecha >= DATE_SUB(CURDATE(), INTERVAL 6 MONTH) ORDER BY Fecha DESC, Hora DESC;";
+            $sql = "SELECT CONCAT(D.Nombre, ' ', D.Apellido) AS Docente, D.ID_Docente, D.Cedula, CASE WHEN LD.Tipo = 'E' THEN 'Entrada' WHEN LD.Tipo = 'S' THEN 'Salida' END AS Tipo, DATE_FORMAT(LD.Fecha, '%d/%m/%Y') AS Fecha, LD.Hora FROM docentes D JOIN lista_docente LD ON LD.ID_Docente = D.ID_Docente WHERE LD.Fecha >= DATE_SUB(CURDATE(), INTERVAL 6 MONTH) ORDER BY Fecha DESC, Hora DESC;";
             break;
         case 'all':
-            $sql = "SELECT CONCAT(D.Nombre, ' ', D.Apellido) AS Docente, D.ID_Docente, D.Cedula, CASE WHEN LD.Tipo = 'E' THEN 'Entrada' WHEN LD.Tipo = 'S' THEN 'Salida' END AS Tipo, LD.Fecha, LD.Hora FROM docentes D JOIN lista_docente LD ON LD.ID_Docente = D.ID_Docente ORDER BY Fecha DESC, Hora DESC;";
+            $sql = "SELECT CONCAT(D.Nombre, ' ', D.Apellido) AS Docente, D.ID_Docente, D.Cedula, CASE WHEN LD.Tipo = 'E' THEN 'Entrada' WHEN LD.Tipo = 'S' THEN 'Salida' END AS Tipo, DATE_FORMAT(LD.Fecha, '%d/%m/%Y') AS Fecha, LD.Hora FROM docentes D JOIN lista_docente LD ON LD.ID_Docente = D.ID_Docente ORDER BY Fecha DESC, Hora DESC;";
             break;
     }
 } else {
-    $sql = "SELECT CONCAT(D.Nombre, ' ', D.Apellido) AS Docente, D.ID_Docente, D.Cedula, CASE WHEN LD.Tipo = 'E' THEN 'Entrada' WHEN LD.Tipo = 'S' THEN 'Salida' END AS Tipo, LD.Fecha, LD.Hora FROM docentes D JOIN lista_docente LD ON LD.ID_Docente = D.ID_Docente WHERE LD.Fecha=curdate() ORDER BY Fecha DESC, Hora DESC;";
+    $docente = isset($_GET['docente']) ? $_GET['docente'] : '';
+    $fecha = isset($_GET['fecha']) ? $_GET['fecha'] : '';
+    $tipo = isset($_GET['tipo']) ? $_GET['tipo'] : '';
+    $hora = isset($_GET['hora']) ? $_GET['hora'] : '';
+    $rangofecha1 = isset($_GET['rangofecha1']) ? $_GET['rangofecha1'] : '';
+    $rangofecha2 = isset($_GET['rangofecha2']) ? $_GET['rangofecha2'] : '';
+    $rangohora1 = isset($_GET['rangohora1']) ? $_GET['rangohora1'] : '';
+    $rangohora2 = isset($_GET['rangohora2']) ? $_GET['rangohora2'] : '';
+    $sql = "SELECT CONCAT(D.Nombre, ' ', D.Apellido) AS Docente, D.ID_Docente, D.Cedula, 
+            CASE WHEN LD.Tipo = 'E' THEN 'Entrada' WHEN LD.Tipo = 'S' THEN 'Salida' END AS Tipo, 
+            DATE_FORMAT(LD.Fecha, '%d/%m/%Y') AS Fecha, LD.Hora 
+            FROM docentes D 
+            JOIN lista_docente LD ON LD.ID_Docente = D.ID_Docente 
+            WHERE 1=1";
+
+    // Añadir filtros opcionales
+    if (!empty($docente)) {
+        $sql .= " AND (CONCAT(D.Nombre, ' ', D.Apellido) LIKE '%$docente%' OR D.Cedula = '$docente')";
+    }
+    
+
+    if (!empty($fecha)) {
+        $sql .= " AND LD.Fecha = '$fecha'";
+    }
+
+    if (!empty($tipo)) {
+        $sql .= " AND LD.Tipo = '$tipo'";
+    }
+
+    if (!empty($hora)) {
+        $sql .= " AND LD.Hora LIKE '%$hora%'";
+    }
+
+    if (!empty($rangofecha1) && !empty($rangofecha2)) {
+        $sql .= " AND LD.Fecha BETWEEN '$rangofecha1' AND '$rangofecha2'";
+    } elseif (!empty($rangofecha1)) {
+        $sql .= " AND LD.Fecha >= '$rangofecha1'";
+    } elseif (!empty($rangofecha2)) {
+        $sql .= " AND LD.Fecha <= '$rangofecha2'";
+    }
+    
+    if (!empty($rangohora1) && !empty($rangohora2)) {
+        $sql .= " AND LEFT(LD.Hora, 5) BETWEEN '$rangohora1' AND '$rangohora2'";
+    } elseif (!empty($rangohora1)) {
+        $sql .= " AND LEFT(LD.Hora, 5) >= '$rangohora1'";
+    } elseif (!empty($rangohora2)) {
+        $sql .= " AND LEFT(LD.Hora, 5) <= '$rangohora2'";
+    }
+    
+    $sql .= " ORDER BY LD.Fecha DESC, LD.Hora DESC;";
+    /*$sql = "SELECT CONCAT(D.Nombre, ' ', D.Apellido) AS Docente, D.ID_Docente, D.Cedula, CASE WHEN LD.Tipo = 'E' THEN 'Entrada' WHEN LD.Tipo = 'S' THEN 'Salida' END AS Tipo, LD.Fecha, LD.Hora FROM docentes D JOIN lista_docente LD ON LD.ID_Docente = D.ID_Docente WHERE LD.Fecha=curdate() ORDER BY Fecha DESC, Hora DESC;";*/
 }
 
 $resultado = mysqli_query($conexion, $sql);
+if (!$resultado) {
+    echo "Error en la consulta: " . mysqli_error($conexion);
+    exit;
+}
 if(mysqli_num_rows($resultado) > 0){
     $lista = mysqli_fetch_all($resultado, MYSQLI_ASSOC);
     echo json_encode($lista);
