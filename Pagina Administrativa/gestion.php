fec6cc71e199253cf7b039515af4c814905e21a9
Vhaur <164172097+Vhaur@users.noreply.github.com>
2024-10-18 16:49:33 -0300

    Avances

--- Pagina Administrativa/gestion.php
+++ Pagina Administrativa/gestion.php
@@ -1,27 +1,45 @@
 <?php
     include("php/header_sidebar.php");
+    include("../BD/conexionbd.php");
+    
+    $pagina = $_GET['pagina'];
+
+    $sql = "SELECT (SELECT COUNT(*) FROM alumnos WHERE Estado = 1) AS totalAlumnos,
+                   (SELECT COUNT(*) FROM clase WHERE Estado = 1) AS totalClases,
+                   (SELECT COUNT(*) FROM docentes WHERE Estado = 1) AS totalDocentes";
+    $resultado = mysqli_query($conexion, $sql);
+    if (mysqli_num_rows($resultado) > 0) {
+        while ($fila = mysqli_fetch_assoc($resultado)) {
+            $cantA = $fila['totalAlumnos'];
+            $cantD = $fila['totalDocentes'];
+            $cantC = $fila['totalClases'];
+        }
+    }
 ?>
 <link rel="stylesheet" href="css/stylegestion.css">
 <div class="gestion">
     <div class="menuGestion">
-        <button id="inicio">Inicio</button>
-        <button id="docentes">Docentes</button>
-        <button id="alumnos">Alumnos</button>
-        <button id="informes">Informes</button>
-        <button id="elementos">Elementos</button>
+        <a href="gestion.php?pagina=inicio" >Inicio</a>
+        <a href="gestion.php?pagina=docentes">Docentes</a>
+        <a href="gestion.php?pagina=alumnos">Alumnos</a>
+        <a href="gestion.php?pagina=informes">Informes</a>
+        <a href="gestion.php?pagina=elementos">Elementos</a>
     </div>
     <div class="gestionContent" id="gestionContent">
+        <?php
+            if($pagina == "inicio") {
+        ?>
         <div>
             <p>Alumnos totales</p>
-            <span id="cantAlumnos"></span>
+            <span id="cantAlumnos"><?php echo $cantA ?></span>
         </div>
         <div>
             <p>Docentes totales</p>
-            <span id="cantDocentes"></span>
+            <span id="cantDocentes"><?php echo $cantD ?></span>
         </div>
         <div>
             <p>Clases totales</p>
-            <span id="cantClases"></span>
+            <span id="cantClases"><?php echo $cantC ?></span>
         </div>
         <div>
             <p>Informes realizados</p>
@@ -41,9 +59,31 @@
                 <canvas id="canvaEsp"></canvas>
             </div>
         </div>
+        <?php
+            }
+            if ($pagina == "docentes") {
+        ?>
+        <p>aca va docentes</p>
+        <?php
+            }
+            if ($pagina == "alumnos") {
+        ?>
+        <p>aca va alumnos</p>
+        <?php
+            }
+            if ($pagina == "informes") {
+        ?>
+        <p>aca va informes</p>
+        <?php
+            }
+            if ($pagina == "elementos") {
+        ?>
+        <p>aca va elementos</p>
+        <?php
+            }
+        ?>
     </div>
 </div>
-<script src="js/gestion.js"></script>
 <?php
     include("php/footer.php");
 ?>
