646a2ba2f43c7398db39f6736ced37a01fb1978d
Luis030 <164172346+Luis030@users.noreply.github.com>
2024-11-09 15:42:10 -0300

    arreglo de consulta

--- BD/datos/cerbd.sql
+++ BD/datos/cerbd.sql
@@ -21,8 +21,8 @@ SET time_zone = "+00:00";
 -- Base de datos: `cerbd`
 --
 
-CREATE DATABASE IF NOT EXISTS cerbd
-USE cerbd
+CREATE DATABASE IF NOT EXISTS cerbd;
+USE cerbd;
 
 -- --------------------------------------------------------
 
--- Pagina Administrativa/php/alumnosclase.php
+++ Pagina Administrativa/php/alumnosclase.php
@@ -5,7 +5,7 @@
         $idclase = $_POST['id'];
 
         if($idclase){
-            $sql = "SELECT A.ID_Alumno, A.Nombre, A.Apellido, A.Cedula, A.Fecha_nac, U.Correo AS Mail_Padres, Celular_Padres, (SELECT COUNT(*) FROM alumnos_clase AC2 WHERE AC2.ID_Alumno = A.ID_Alumno AND AC2.ID_Clase = 13 AND AC2.Asistio = 0 AND AC2.Estado = 1) AS Faltas FROM alumnos A, alumnos_clase AC, clase C, usuarios U WHERE A.ID_Alumno = AC.ID_Alumno AND AC.ID_Clase = C.ID_Clase AND C.ID_Clase = 13 AND AC.Estado = 1 AND A.ID_Usuario = U.ID_Usuario AND AC.Asistio IS NULL AND A.Estado = 1";
+            $sql = "SELECT A.ID_Alumno, A.Nombre, A.Apellido, A.Cedula, A.Fecha_nac, U.Correo AS Mail_Padres, Celular_Padres, (SELECT COUNT(*) FROM alumnos_clase AC2 WHERE AC2.ID_Alumno = A.ID_Alumno AND AC2.ID_Clase = $idclase AND AC2.Asistio = 0 AND AC2.Estado = 1) AS Faltas FROM alumnos A, alumnos_clase AC, clase C, usuarios U WHERE A.ID_Alumno = AC.ID_Alumno AND AC.ID_Clase = C.ID_Clase AND C.ID_Clase = $idclase AND AC.Estado = 1 AND A.ID_Usuario = U.ID_Usuario AND AC.Asistio IS NULL AND A.Estado = 1";
             $resultado = mysqli_query($conexion, $sql);
             if(mysqli_num_rows($resultado) > 0){
                 $envio = mysqli_fetch_all($resultado, MYSQLI_ASSOC);
